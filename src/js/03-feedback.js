c0f3a8902995815f6f057755fe12b00ec013fd00
Istomina Alena <mynikisalena@gmail.com>
2021-11-16 09:42:49 +0200

    последний пуш

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,35 @@
-import throttle from 'lodash/throttle';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[type="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
-const savedData = localStorage.getItem('feedback-form-state');
+form.addEventListener('input', throttle(localStorageFn, 500));
+
+const dataObj = {};
+
+function localStorageFn(evt) {
+  dataObj[emailInput.name] = emailInput.value;
+  dataObj[messageInput.name] = messageInput.value;
+  // dataObj[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
+}
+
+const savedData = localStorage.getItem(STORAGE_KEY);
 const parsedData = JSON.parse(savedData);
 
+form.addEventListener('submit', onFormClear);
+
+function onFormClear(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
 function localStorageCheck() {
-  if (localStorage.getItem('feedback-form-state')) {
+  if (localStorage.getItem(STORAGE_KEY)) {
     emailInput.value = parsedData.email;
     messageInput.value = parsedData.message;
   }
@@ -16,22 +37,3 @@ function localStorageCheck() {
 }
 
 localStorageCheck();
-
-form.addEventListener('input', throttle(localStorageFn, 500));
-
-const dataObj = {};
-
-function localStorageFn() {
-  dataObj[emailInput.name] = emailInput.value;
-  dataObj[messageInput.name] = messageInput.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(dataObj));
-}
-
-form.addEventListener('submit', onFormClear);
-
-function onFormClear(evt) {
-  evt.preventDefault();
-  emailInput.value = '';
-  messageInput.value = '';
-  localStorage.removeItem('feedback-form-state');
-}

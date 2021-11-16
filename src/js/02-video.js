8e3482697666db7e6930acb71c32f08017c58981
Istomina Alena <mynikisalena@gmail.com>
2021-11-16 17:29:45 +0200

    все!)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,8 @@
 import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
-function localStorageCheck() {
-  if (localStorage.length !== 0) {
-    onPlay();
-    return;
-  }
-  return;
-}
 
 function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
@@ -19,8 +12,6 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
 
-console.log(savedTime);
-
 let parsedTime;
 
 try {
@@ -29,22 +20,10 @@ try {
   console.log(error.name);
   console.log(error.message);
 }
-console.log(parsedTime);
 
-player
-  .setCurrentTime(parsedTime)
-  .then(function (seconds) {
-    console.log(seconds);
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        console.log(error.message);
-        break;
-
-      default:
-        console.log(error.name);
-        console.log(error.message);
-        break;
-    }
-  });
+if (parsedTime) {
+  player
+    .setCurrentTime(parsedTime)
+    .then(parsedTime => console.log(parsedTime))
+    .catch(error => console.log(`${error.name}:`, error.message));
+}
